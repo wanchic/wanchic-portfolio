7fb6347d43ec548cbb9eeb92477a71a25da4b03d
Eric Wanchic <eric@gratiel.com>
2026-03-10 19:05:42 -0500

    Add database to rspec and remove minitest

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,17 @@ module WanchicPortfolio
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: true,
+                       controller_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+      g.system_tests = nil
+    end
   end
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -2,6 +2,6 @@
 
 module WanchicPortfolio
   class Application
-    VERSION = "26.310.1441c"
+    VERSION = "26.310.1905c"
   end
 end
