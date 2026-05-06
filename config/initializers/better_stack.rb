7370473cfda74ac1d5cdfc54e0f768d43eb6b4a3
Eric Wanchic <eric@gratiel.com>
2026-05-05 22:12:40 -0500

    Reposition the Version loading

--- config/initializers/better_stack.rb
+++ config/initializers/better_stack.rb
@@ -1,5 +1,9 @@
 if Rails.env.production?
-  Rails.logger = Logtail::Logger.create_default_logger(
-    source_token: ENV.fetch("BETTER_STACK_SOURCE_TOKEN")
-  )
+  if ENV.fetch("BETTER_STACK_SOURCE_TOKEN", nil).present?
+    Rails.logger = Logtail::Logger.create_default_logger(
+      source_token: ENV.fetch("BETTER_STACK_SOURCE_TOKEN", nil)
+    )
+  else
+    Rails.logger.info "Better Stack disabled: BETTER_STACK_SOURCE_TOKEN is not set"
+  end
 end
